dff582a90a390bd13de3441c4bbdae26d33754ba
Vespertili0 <97523486+Vespertili0@users.noreply.github.com>
2025-04-08 10:28:38 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mlipflow',
+    version='0.2',
+    description="Code for generating MLIPs using wfl",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True
+)
