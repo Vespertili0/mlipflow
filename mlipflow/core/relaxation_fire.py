314871f14d90ca0b4e744e96808c4d4cf574315a
Vespertili0 <s.jeschke@griffith.edu.au>
2026-02-05 17:29:37 +0000

    trimmed relaxation_fire removing bulk modelling features

--- mlipflow/core/relaxation_fire.py
+++ mlipflow/core/relaxation_fire.py
@@ -1,22 +1,18 @@
 import sys
-import ase.units
-import numpy as np
-import spglib
-from ase.filters import FrechetCellFilter
 from ase.optimize import FIRE
 from wfl.autoparallelize import autoparallelize, autoparallelize_docstring
 from wfl.utils.save_calc_results import at_copy_save_calc_results
 from wfl.utils.misc import atoms_to_list
 from wfl.utils.parallel import construct_calculator_picklesafe
-from wfl.utils.pressure import sample_pressure
 from wfl.generate.utils import save_config_type
 
 
-def _run_autopara_wrappable(atoms, calculator, fmax=1.0e-3, smax=None, steps=1000, pressure=None,
-           stress_mask=None, keep_symmetry=True, traj_step_interval=1, traj_subselect=None,
-           skip_failures=True, results_prefix='last_op__optimize_', verbose=False, update_config_type="append",
-           rng=None, _autopara_per_item_info=None,
-           **opt_kwargs):
+def _run_autopara_wrappable(
+        atoms, calculator, fmax=1.0e-3, smax=None, steps=1000, traj_step_interval=1, traj_subselect=None,
+        skip_failures=True, results_prefix='last_op__optimize_', verbose=False, update_config_type="append",
+        rng=None, _autopara_per_item_info=None,
+        **opt_kwargs
+    ):
     """runs a structure optimization. By default calculator properties will be stored in keys
     prefixed with "last_op__optimize_", which may be overwritten by next operation.
 
@@ -68,58 +64,19 @@ def _run_autopara_wrappable(atoms, calculator, fmax=1.0e-3, smax=None, steps=100
     """
     calculator = construct_calculator_picklesafe(calculator)
 
-    if smax is None:
-        smax = fmax
-
-    if keep_symmetry:
-        # noinspection PyUnresolvedReferences,PyUnresolvedReferences
-        try:
-            from ase.constraints import FixSymmetry
-        except ImportError:
-            # fall back to previous import location (pre MR 3288)
-            from ase.spacegroup.symmetrize import FixSymmetry
-
     all_trajs = []
 
     for at_i, at in enumerate(atoms_to_list(atoms)):
-        # get rng from autopara_per_item info if available ("rng" arg that was passed in was
-        # already used by autoparallelization framework to set "rng" key in per-item dict)
-        rng = _autopara_per_item_info[at_i].get("rng")
-
         # original constraints
         org_constraints = at.constraints
-
-        if keep_symmetry:
-            sym = FixSymmetry(at, adjust_cell=False)
-            # Append rather than overwrite constraints
-            at.set_constraint([*at.constraints, sym])
-
-            dataset = spglib.get_symmetry_dataset((at.cell, at.get_scaled_positions(), at.numbers), 0.01)
-            if 'buildcell_config_i' in at.info:
-                print(at.info['buildcell_config_i'], end=' ')
-            print('initial symmetry group number {}, international (Hermann-Mauguin) {} Hall {} prec {}'.format(
-                dataset.number, dataset.international, dataset.hall, 0.01))
-
         at.calc = calculator
-        if pressure is not None:
-            p = sample_pressure(pressure, at, rng=rng)
-            at.info['optimize_pressure_GPa'] = p
-            p *= ase.units.GPa
-            wrapped_at = FrechetCellFilter(at, scalar_pressure=p, mask=stress_mask)
-        else:
-            wrapped_at = at
-
-        opt = FIRE(wrapped_at, logfile=sys.stdout if verbose else None)
+        opt = FIRE(at, logfile=sys.stdout if verbose else None)
 
         # default status, will be overwritten for first and last configs in traj
         at.info['optimize_config_type'] = 'optimize_mid'
         traj = []
 
         def process_step():
-            if 'RSS_min_vol_per_atom' in at.info and at.get_volume() / len(at) < at.info['RSS_min_vol_per_atom']:
-                raise RuntimeError('Got volume per atom {} under minimum {}'.format(at.get_volume() / len(at),
-                                                                                    at.info['RSS_min_vol_per_atom']))
-
             if len(traj) > 0 and traj[-1] == at:
                 # Some optimization algorithms sometimes seem to repeat, perhaps
                 # only in weird circumstances, e.g. bad gradients near breakdown.
@@ -162,18 +119,6 @@ def _run_autopara_wrappable(atoms, calculator, fmax=1.0e-3, smax=None, steps=100
 
         traj[-1].info['optimize_config_type'] = f'optimize_last_{final_status}'
         traj[-1].info['optimize_n_steps'] = opt.get_number_of_steps()
-
-        if keep_symmetry:
-            # should we check that initial is subgroup of final, i.e. no symmetry was lost?
-            dataset = spglib.get_symmetry_dataset((traj[-1].cell, traj[-1].get_scaled_positions(), traj[-1].numbers), 0.01)
-            if 'buildcell_config_i' in at.info:
-                print(at.info['buildcell_config_i'], end=' ')
-            if dataset is None:
-                print('final symmetry group number None')
-            else:
-                print('final symmetry group number {}, international (Hermann-Mauguin) {} Hall {} prec {}'.format(
-                    dataset.number, dataset.international, dataset.hall, 0.01))
-
 
         for at in traj:
             save_config_type(at, update_config_type, at.info["optimize_config_type"])
